6ceb3277eefdd0c85b03f240e655600a2988f943
viktor286 <just.viktor@gmail.com>
2022-07-14 13:36:20 -0400

    add TimezonesSelector

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
 import './App.css';
+import TimezonesSelector from './components/TimezoneSelector';
 
 function App() {
-  return <div className="App">Timezones App</div>;
+  return (
+    <div className="App">
+      <h1>Timezones Clock ⏰</h1>
+      <TimezonesSelector />
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/TimezoneSelector/index.js
@@ -0,0 +1,14 @@
+import './index.css';
+
+// const timezones = Intl.supportedValuesOf('timeZone');
+
+export default function TimezonesSelector() {
+  return (
+    <section className="timezones-selector">
+      <div className="adaptive-box">
+        <input type="text" />
+        {/* AutocompleteDropdown */}
+      </div>
+    </section>
+  );
+}
